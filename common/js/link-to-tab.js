3d6e7897646ce467f97e2e13ede0ae5715a1268f
doublesharp <jsilver@doublesharp.com>
2015-11-15 13:09:59 -0800

    Listen for changes to the hash value and try to click tabs

--- common/js/link-to-tab.js
+++ common/js/link-to-tab.js
@@ -10,23 +10,38 @@
 			$(document).one('acf/fields/tab/refresh', processLocationHash);
 		}
 
+		// Click tab based on the hash changing
+		$(window).on('hashchange', processLocationHash);
+
+		// We want to track hash changes when a tab is clicked, but not click it again
+		var ignoreHashChange = false;
+
 		// If there is a location hash, try to use it to click a tab
 		function processLocationHash(){
-			if (location.hash.length>1){	
-				var hash = location.hash.substring(1);
-				$('.acf-tab-wrap .acf-tab-button').each(function(i, button){ 
-					if (hash==$(button).text().toLowerCase().replace(' ', '-')){
-						$(button).trigger('click');
-
-						//acf.unload.changed = 0;
-						$(window).off('beforeunload');
-						return false;
-					}
-				});
+			if (!ignoreHashChange && location.hash.length>1){
+				clickTabByName(location.hash.slice(1));
 			}
 		}
+
+		// Look for all the tabs and click the first name that matches the hash
+		function clickTabByName(name){
+			$('.acf-tab-wrap .acf-tab-button').each(function(i, button){ 
+				if (name==$(button).text().toLowerCase().replace(' ', '-')){
+					$(button).trigger('click');
+
+					//acf.unload.changed = 0;
+					$(window).off('beforeunload');
+					return false;
+				}
+			});
+		}
+
 		// Add location hash when tab is clicked
 		$(document).on('click', '.acf-tab-wrap .acf-tab-button', function($el){
+			ignoreHashChange = true;
 			location.hash='#'+$(this).text().toLowerCase().replace(' ', '-');
+			ignoreHashChange = false;
 		});
+
 })(jQuery);
+
